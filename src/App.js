a65c3df5f5ebf69765fcc0c44767f7b65c4775ca
Sourabh Sonawat <sourabh8820@gmail.com>
2023-04-26 10:51:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SimpleInput from './components/SimpleInput';
+import BasicInput from './components/BasicForm';
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+      {/* <BasicInput /> */}
+    </div>
+  );
+}
+
+export default App;
